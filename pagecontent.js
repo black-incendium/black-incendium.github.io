62ace0f3961a97afe7e73f6820e4f38c7e248c55
black-incendium <83142442+black-incendium@users.noreply.github.com>
2021-04-29 23:15:14 +0200

    add pagecontent.js for future changes in layout

---
+++ pagecontent.js
@@ -0,0 +1,28 @@
+let pagecontent = {};
+
+pagecontent.slogancontainer = '<div class="sloganinnercontainer"><div class="slogan1"><span class="slogan1staticpart">Looking for someone who </span><span class="typingeffectspan">really understand JavaScript?</span><span class="line lineborder"></span></div><div class="slogan2">Look no more, <span class="contactspan">contact me</span>.</div></div>';
+
+/*<div class="sloganinnercontainer">
+	<div class="slogan1"><span class="slogan1staticpart">Looking for someone who </span><span class="typingeffectspan">really understand JavaScript?</span><span class="line lineborder"></span></div>
+	<div class="slogan2">Look no more, <span class="contactspan">contact me</span>.</div>
+</div>*/
+
+pagecontent.projectscontainer = '<div class="innerprojectscontainer">projects</div>';
+
+/*<div class="innerprojectscontainer">projects</div>*/
+
+pagecontent.contactcontainer = '<div class="innercontactcontainer"><a href="https://github.com/black-incendium" target="_blank"><div class="githubcontact"></div></a><a href="https://www.linkedin.com/in/jakub-ko%C5%82odziej-916880210/" target="_blank"><div class="linkedincontact"></div></a><a href="mailto:contact@jakubkolodziej.com"><div class="emailcontact"></div></a></div>';
+
+/*<div class="innercontactcontainer">
+	<a href="https://github.com/black-incendium" target="_blank"><div class="githubcontact"></div></a>
+	<a href="https://www.linkedin.com/in/jakub-ko%C5%82odziej-916880210/" target="_blank"><div class="linkedincontact"></div></a>
+	<a href="mailto:contact@jakubkolodziej.com"><div class="emailcontact"></div></a>
+</div>*/
+
+pagecontent.aboutme = '<div class="inneraboutmecontainer"><p>Nazywam się <span class="bluespan">Jakub Kołodziej.</span></p><p><span class="under500pxblock">Jestem aspirującym </span><span class="redspan">JavaScript Junior Developerem.</span></p><p>Jestem absolwentem XIV LO im. Stanisława Staszica w Warszawie, czyli liceum najczęściej wygrywającego tytuł <span class="redspan">najlepszego liceum w Polsce</span>. Niedługo zaczynam również studia zaoczne na kierunku informatyka. Moje najważniejsze cechy to to, że <span class="bluespan">chcę, umiem i lubię się uczyć</span>, przez co jestem w stanie szybko opanować skomplikowane zagadnienia. JavaScript oraz ogół programowania jest nie tylko moim zajęciem w związku z pracą, ale także <span class="redspan">głównym zainteresowaniem</span> i <span class="redspan">pasją</span>, dzięki czemu ciągle chcę i jestem w stanie się <span class="redspan">rozwijać</span>. Przy nauce JavaScriptu postawiłem nie na <span class="bluespan">zapamiętywanie funkcji jednego frameworka</span>, ale na <span class="redspan">zrozumienie</span> jak ten język naprawdę działa i dlaczego. Dzięki temu jestem w stanie opanowywać nowe zagadnienia o wiele szybciej i z większym zrozumieniem. Posiadam również bardzo mocne fundamenty w postaci <span class="redspan">wiedzy i umiejętności matematycznych</span> - jestem laureatem wielu konkursów z tej dziedziny. W wolnym czasie prowadzę i rozwijam własne projekty.</p></div>';
+
+/*<div class="inneraboutmecontainer">
+	<p>Nazywam się <span class="bluespan">Jakub Kołodziej.</span></p>
+	<p><span class="under500pxblock">Jestem aspirującym </span><span class="redspan">JavaScript Junior Developerem.</span></p>
+	<p>Jestem absolwentem XIV LO im. Stanisława Staszica w Warszawie, czyli liceum najczęściej wygrywającego tytuł <span class="redspan">najlepszego liceum w Polsce</span>. Niedługo zaczynam również studia zaoczne na kierunku informatyka. Moje najważniejsze cechy to to, że <span class="bluespan">chcę, umiem i lubię się uczyć</span>, przez co jestem w stanie szybko opanować skomplikowane zagadnienia. JavaScript oraz ogół programowania jest nie tylko moim zajęciem w związku z pracą, ale także <span class="redspan">głównym zainteresowaniem</span> i <span class="redspan">pasją</span>, dzięki czemu ciągle chcę i jestem w stanie się <span class="redspan">rozwijać</span>. Przy nauce JavaScriptu postawiłem nie na <span class="bluespan">zapamiętywanie funkcji jednego frameworka</span>, ale na <span class="redspan">zrozumienie</span> jak ten język naprawdę działa i dlaczego. Dzięki temu jestem w stanie opanowywać nowe zagadnienia o wiele szybciej i z większym zrozumieniem. Posiadam również bardzo mocne fundamenty w postaci <span class="redspan">wiedzy i umiejętności matematycznych</span> - jestem laureatem wielu konkursów z tej dziedziny. W wolnym czasie prowadzę i rozwijam własne projekty.</p>
+</div>*/
