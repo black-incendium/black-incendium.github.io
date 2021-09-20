335d55dc3a6646caf3111291ba1903a31fd53751
black-incendium <cryingincendium@gmail.com>
2021-09-20 11:49:32 +0200

    optimised background particles

--- script.js
+++ script.js
@@ -4,96 +4,9 @@ canvas.width = window.innerWidth*2;
 canvas.height = window.innerHeight*2;
 canvas.style = "transform: scale(0.5)";
 
-let cursor = {
-	x: -1000,
-	y: -1000
-}
-
-let options = {
-	firstColor: "white",
-	secondColor: "black"
-}
-
-let particles = {
-	list: [],
-	update: function() {
-		particles.list.forEach(function(el, index, array) {
-			el.update();
-		});
-		particles.draw();
-		setTimeout(particles.update, 20);
-	},
-	create: function(obj={}) {
-		let newObj = Object.create(particleDelegationObject);
-		newObj.speedX = obj.speedX ?? Math.random()*2-1;
-		newObj.speedY = obj.speedY ?? Math.random()*2-1;
-		newObj.color = obj.color ?? options.firstColor;
-		newObj.radius = obj.radius ?? 2;
-		newObj.x = obj.x ?? Math.random()*canvas.width;
-		newObj.y = obj.y ?? Math.random()*canvas.height;
-		particles.list.push(newObj);
-	},
-	draw: function() {
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-		particles.list.forEach(function(el, index, array) {
-			let distanceFromCursor = Math.sqrt((el.x - cursor.x)*(el.x - cursor.x) + (el.y - cursor.y)*(el.y - cursor.y));
-			let graynessForCursor = -1*distanceFromCursor*(128/(Math.min(canvas.width, canvas.height)/7))+128;
-			let colorForCursor = `rgba(${graynessForCursor/2}, ${graynessForCursor*2}, ${graynessForCursor*2}, 255)`;
-			if (distanceFromCursor < Math.min(canvas.width, canvas.height)/7) {
-				canvasAdditionalFunctions.line({x1: el.x, y1: el.y, x2: cursor.x, y2: cursor.y, color: colorForCursor});
-			}
-			particles.list.forEach(function (el2, index2, array2) {
-				let distance = Math.sqrt((el.x - el2.x)*(el.x - el2.x) + (el.y - el2.y)*(el.y - el2.y));
-				let grayness = -1*distance*(128/(Math.min(canvas.width, canvas.height)/7))+128;
-				let color = `rgba(${grayness/2}, ${grayness*2}, ${grayness*2}, 255)`;
-				if (distance < Math.min(canvas.width, canvas.height)/7) {
-					canvasAdditionalFunctions.line({x1: el.x, y1: el.y, x2: el2.x, y2: el2.y, color: color});
-				}
-			});
-		});
-	}
-}
-
-let canvasAdditionalFunctions = {
-	circle: function(obj={}, context) {
-		context = (context ?? ctx);
-		context.beginPath();
-		context.arc(
-			obj.x ?? canvas.width/2,
-			obj.y ?? canvas.height/2,
-			obj.r ?? 10,
-			0,
-			2*Math.PI);
-		context.fillStyle = obj.color ?? options.firstColor;
-		context.fill();
-		context.closePath();
-	},
-	line: function(obj={}, context) {
-		context = (context ?? ctx);
-		context.beginPath();
-		context.moveTo(obj.x1, obj.y1);
-		context.lineTo(obj.x2, obj.y2);
-		context.lineWidth = 1;	
-		context.strokeStyle = obj.color ?? options.firstColor;
-		context.stroke();
-		context.closePath();
-	}
-}
-
-let particleDelegationObject = {
-	update: function() {
-		this.x+=this.speedX;
-		this.y+=this.speedY;
-		if (this.x < 0 + this.radius) {this.x = 0 + this.radius; this.speedX *= -1;}
-		if (this.y < 0 + this.radius) {this.y = 0 + this.radius; this.speedY *= -1;}
-		if (this.x > canvas.width - this.radius) {this.x = canvas.width - this.radius; this.speedX *= -1;}
-		if (this.y > canvas.height - this.radius) {this.y = canvas.height - this.radius; this.speedY *= -1;}
-	}
-}
-
-document.addEventListener("mousemove", function(e) {
-	cursor.x = e.clientX*2;
-	cursor.y = e.clientY*2;
+window.addEventListener("resize", function() {
+	canvas.width = document.body.clientWidth*2;
+	canvas.height = document.body.clientHeight*2;
 });
 
 let dynamicallyTypedText = {
@@ -162,7 +75,105 @@ document.querySelector(".contactspan").addEventListener("click", function(e){
 	document.querySelector(`.contactcontainer`).classList.add("shown");
 });
 
-for (let i=0; i<window.innerHeight*window.innerWidth/10000; i++) {particles.create();}
+let cursor = {
+	x: -1000,
+	y: -1000
+}
+
+let options = {
+	firstColor: "white",
+	secondColor: "black"
+}
+
+document.addEventListener("mousemove", function(e) {
+	cursor.x = e.clientX*2;
+	cursor.y = e.clientY*2;
+});
+
+let particles = (function(){
+	function line(obj={}, context) {
+		context = (context ?? ctx);
+		context.beginPath();
+		context.moveTo(obj.x1, obj.y1);
+		context.lineTo(obj.x2, obj.y2);
+		context.lineWidth = 1;	
+		context.strokeStyle = obj.color ?? options.firstColor;
+		context.stroke();
+		context.closePath();
+	}
+
+	function circle(obj={}, context) {
+		context = (context ?? ctx);
+		context.beginPath();
+		context.arc(
+			obj.x ?? canvas.width/2,
+			obj.y ?? canvas.height/2,
+			obj.radius ?? 10,
+			0,
+			2*Math.PI);
+		context.fillStyle = obj.color ?? options.firstColor;
+		context.fill();
+		context.closePath();
+	}
+
+	return ({
+		list: [],
+		update: function() {
+			particles.list.forEach(function(el, index, array) {
+				el.update();
+			});
+			particles.draw();
+			setTimeout(particles.update, 20);
+		},
+		create: function(obj={}) {
+			let randomColor = Math.random()*128+64;
+			let newObj = {
+				speedX: obj.speedX ?? Math.random()*6-3,
+				speedY: obj.speedY ?? Math.random()*6-3,
+				color: `rgb(${randomColor},${randomColor},${randomColor})`,
+				radius: Math.random()*3+2,
+				x: obj.x ?? Math.random()*canvas.width,
+				y: obj.y ?? Math.random()*canvas.height,
+				update: function() {
+					newObj.x+=newObj.speedX;
+					newObj.y+=newObj.speedY;
+					if (newObj.x < 0 + newObj.radius) {newObj.x = 0 + newObj.radius; newObj.speedX *= -1;}
+					if (newObj.y < 0 + newObj.radius) {newObj.y = 0 + newObj.radius; newObj.speedY *= -1;}
+					if (newObj.x > canvas.width - newObj.radius) {newObj.x = canvas.width - newObj.radius; newObj.speedX *= -1;}
+					if (newObj.y > canvas.height - newObj.radius) {newObj.y = canvas.height - newObj.radius; newObj.speedY *= -1;}
+				}
+			};
+			particles.list.push(newObj);
+		},
+		draw: function() {
+			let d = 400;
+			ctx.clearRect(0,0,canvas.width,canvas.height);
+			particles.list.forEach(function(el) {
+				let distanceFromCursor = Math.sqrt((el.x - cursor.x)*(el.x - cursor.x) + (el.y - cursor.y)*(el.y - cursor.y));
+				let graynessForCursor = -1*distanceFromCursor*(128/d)+128;
+				let colorForCursor = `rgba(${graynessForCursor/2}, ${graynessForCursor*2}, ${graynessForCursor*2}, 255)`;
+				if (distanceFromCursor < d) {
+					line({x1: el.x, y1: el.y, x2: cursor.x, y2: cursor.y, color: colorForCursor});
+				}
+				particles.list.forEach(function (el2) {
+					let distance = Math.sqrt((el.x - el2.x)*(el.x - el2.x) + (el.y - el2.y)*(el.y - el2.y));
+					let grayness = -1*distance*(128/d)+128;
+					let color = `rgba(${grayness/2}, ${grayness*2}, ${grayness*2}, 255)`;
+					if (distance < d) {
+						line({x1: el.x, y1: el.y, x2: el2.x, y2: el2.y, color: color});
+					}
+				});
+			});
+			particles.list.forEach(function(el) {
+				circle(el, ctx);
+			});
+		}
+	});
+})();
+
+for (let i=0; i<Math.sqrt(canvas.width*canvas.height)/100; i++) {
+	particles.create();
+}
 particles.update();
 setTimeout(dynamicallyTypedText.update, dynamicallyTypedText.speed*25);
 dynamicallyTypedText.cursorLineUpdate();
